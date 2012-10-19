a5cd9714b9dd57dee0ed1c4c679895c91760af82
yyyurik <7-8@tut.by>
2012-10-19 15:15:52 +0300

    Удалил нижний блок на гланой странице сайта

--- blocks/block_aside.php
+++ blocks/block_aside.php
@@ -6,30 +6,10 @@
                 <div class="wrapper">
                     <?php echo $aside->getTopNews(); ?>
                 </div>
-                <div class="wrapper">
-                    <div class="grid_7 alpha">
-                        <ul class="list1">
-                        <?php
-                        $articles_title = $aside->getArticles();
-                        for($i = 0; $i < 4; $i++) {
-                            echo $articles_title[$i];
-                        }
-                        ?>
-                        </ul>
-                    </div>
-                    <div class="grid_7 alpha">
-                        <ul class="list1">
-                        <?php
-                        for($i = 4; $i < 8; $i++) {
-                            echo $articles_title[$i];
-                        }
-                        ?>
-                        </ul>
-                    </div>
-                </div>
+
             </div>
             <div class="grid_7 padRT">
-                <h3>Новости</h3>
+                <h3>Наши новости</h3>
                     <?php
                     foreach ($aside->getNews() as $k => $news) {
                         echo $news;
