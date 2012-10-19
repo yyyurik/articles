d7968f774491f61cf6461ea7458fe39368e9c538
yyyurik <7-8@tut.by>
2012-10-19 15:34:11 +0300

    Изменения в дизайне

--- blocks/block_about.php
+++ blocks/block_about.php
@@ -9,29 +9,21 @@
                         echo $value;
                     }
                     ?>
-                    <h3 class="padtop4">Что о нас говорят</h3>
+                <!--    <h3 class="padtop4">Что о нас говорят</h3>
                     <?php
                     foreach ($about->getFeedback() as $key => $value) {
                         echo $value;
                     }
                     ?>
+                    -->
             </div>
             <div class="grid_16 padRT">
                 <div id="padbot3">
-                    <h3>Наша миссия</h3>
+                    <h3>Правила размещения статей</h3>
                     <?php
                         echo $about->getMission();
                     ?>
                 </div>
-                <h3>Лучшие сотрудники</h3>
-                <ul class="list2">
-                    <li>
-                    <li>
-                    <li class="last">
-                    <li>
-                    <li>
-                    <li class="last">
-                </ul>
             </div>
         </div>
     </div>
